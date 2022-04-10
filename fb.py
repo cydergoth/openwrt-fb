8e2667b9289c2fea2dab4de0ec7756f03b9178ec
Da Cydergoth <cydergoth@gmail.com>
2022-04-10 13:16:02 -0500

    Initial graph

--- fb.py
+++ fb.py
@@ -1,89 +1,74 @@
 import os
-import mmap 
+import mmap
+import traceback
 
 class Framebuffer():
-	def __init__(self, fbdev="fb0"):
-		self.fbdev=fbdev
-		with open(f"/sys/class/graphics/{fbdev}/name", "r") as f:
-		   data=f.read()
-		   self.name=data
 
-		# Get screen size
-		with open(f"/sys/class/graphics/{fbdev}/virtual_size", "r") as f:
-			data = f.read()
-			(x,y) = data.split(",")
-			self.x = int(x)
-			self.y = int(y)
+    def __init__(self, fbdev="fb0"):
+        self._fbdev=fbdev
+        self._fb = None
+        self._fb_bytes = None
 
-		# Get bit per pixel
-		with open(f"/sys/class/graphics/{fbdev}/bits_per_pixel", "r") as f:
-         		self.bpp = int(f.read())
+        with open(f"/sys/class/graphics/{fbdev}/name", "r") as f:
+            data=f.read()
+            self._name=data
 
-	def __enter__(self):
-		# Open the framebuffer device
-		self.fbdev = os.open(f"/dev/{self.fbdev}", os.O_RDWR)
+        # Get screen size
+        with open(f"/sys/class/graphics/{fbdev}/virtual_size", "r") as f:
+            data = f.read()
+            (x,y) = data.split(",")
+            self._size = (int(x), int(y))
 
-		# Map framebuffer to memory
-		self.fb = mmap.mmap(self.fbdev, self.x*self.y*self.bpp//8, mmap.MAP_SHARED, mmap.PROT_WRITE|mmap.PROT_READ, offset=0)
-		return self
+        # Get bit per pixel
+        with open(f"/sys/class/graphics/{fbdev}/bits_per_pixel", "r") as f:
+            self._bpp = int(f.read())
 
-	def __exit__(self, exc_type, exc_value, tb):
-		if exc_type is not None:
-			traceback.print_exception(exc_type, exc_value, tb)
-		self.fb.close()
-		os.close(self.fbdev)
 
-	def drawpixel(self, x, y, r, g, b, t=0):
-		'''
-		drawpixel function
-		Draw a single pixel with color
-		Require:
-			x: x coordinate of the pixel
-			y: y coordinate of the pixel
-			
-			RGB color:
-				r: Red color (0 -> 255)
-				g: Green color (0 -> 255)
-				b: Blue color (0 -> 255)
+    @property
+    def name(self):
+        return self._name
 
-			t: transparency (default set to 0)
-		'''
-		self.fb.seek((x+(y*self.x))*(self.bpp//8)) # Set the pixel location
+    @property
+    def fbdev(self):
+        return self._fbdev
 
-		if self.bpp == 32:
-			# 32 bit per pixel
-			self.fb.write(b.to_bytes(1, byteorder='little')) # Write blue
-			self.fb.write(g.to_bytes(1, byteorder='little')) # Write green
-			self.fb.write(r.to_bytes(1, byteorder='little')) # Write red
-			self.fb.write(t.to_bytes(1, byteorder='little')) # Write transparency
+    @property
+    def size(self):
+        return self._size
 
-		else:
-			# 16 bit per pixel
-			self.fb.write(r.to_bytes(1, byteorder='little') << 11 | g.to_bytes(1, byteorder='little') << 5 | b.to_bytes(1, byteorder='little'))
+    @property
+    def bpp(self):
+        return self._bpp
 
-	def clear(self, r=255, g=255, b=255, t=0):
-		'''
-		clear function
-		Clear the screen with color
-		Require:
-			RGB color:
-					r: Red color (0 -> 255)
-					g: Green color (0 -> 255)
-					b: Blue color (0 -> 255)
+    def __enter__(self):
+        # Open the framebuffer device
+        self._fb = os.open(f"/dev/{self._fbdev}", os.O_RDWR)
 
-			t: transparency (default set to 0)
-		'''
-		for y in range(self.y):
-			for x in range(self.x):
-				self.drawpixel(x, y, r, g, b, t)
+        # Map framebuffer to memory
+        (x,y) = self.size
+        self._fb_bytes = mmap.mmap(self._fb, x*y*self.bpp//8, mmap.MAP_SHARED, mmap.PROT_WRITE|mmap.PROT_READ, offset=0)
+        return self
 
-	def write_screen(self,some_bytes):
-		self.fb.write(some_bytes)
+    def __exit__(self, exc_type, exc_value, tb):
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_value, tb)
+        self._fb_bytes.close()
+        os.close(self._fb)
+
+    # fill should be (rgba)
+    def clear(self, fill):
+        (r,g,b,a) = fill
+        mapped = (b, g, r, a)
+        (x,y) = self.size
+        screen = bytearray(bytes(mapped))*x*y
+        self._fb_bytes.write(screen)
+
+    def write_screen(self,some_bytes):
+        self._fb_bytes.seek(0)
+        self._fb_bytes.write(some_bytes)
+
 
 if __name__ == "__main__":
-	fb0 = Framebuffer("/dev/fb0")
-	fb0.clear(r=255,g=255,b=255)
-	#for y in range(fb0.y):
-	#	for x in range(fb0.x):
-	#		fb0.drawpixel(x, y, x%300, y%400, (x+y)%500)
-
+    fb0 = Framebuffer("fb0")
+    with fb0 as display:
+        display.clear((0,255,0,128))
--- network.py
+++ network.py
@@ -1,20 +1,146 @@
+import time
+import logging
 from fb import Framebuffer
 from PIL import Image, ImageDraw
-
+from PIL.ImageColor import getrgb
+from collections import deque
 import psutil
+import asyncio
+from contextlib import suppress
+import datetime
+import traceback
 
-fb = Framebuffer()
+logging.basicConfig(level=logging.INFO)
 
-img = Image.new(mode="RGBA", size=(fb.x,fb.y))
-draw_img = ImageDraw.Draw(img)
 
-data = ['4','5','87','1','44','83','93','2','54','84','100','64'] 
-x = 0
+class IfSampler:
 
-for i in data:
-    x = x + 30  
-    y = 200 - int(i) 
-    draw_img.line((x,200,x,y), width=10, fill=(255,0,0,255))
+    def __init__(self, ifname, sample_len):
+        self._ifname=ifname
+        self._sample_len=sample_len
+        self._buffer=deque(maxlen=sample_len)
+        self._last_sample_ts=datetime.datetime.now()
+        sample=psutil.net_io_counters(pernic=True)[self._ifname]
+        (bytes_sent,bytes_recv,*_) = sample
+        self._last_sample=(bytes_sent, bytes_recv)
+        self._scrape=Periodic(self._do_scrape,5)
 
-with fb as display:
-   display.write_screen(img.tobytes())
+    async def start(self):
+        return await self._scrape.start()
+
+    def _do_scrape(self):
+        sample=psutil.net_io_counters(pernic=True)[self._ifname]
+        (bytes_sent,bytes_recv,*_) = sample
+        (last_sent, last_recv) = self._last_sample
+        sent_delta=bytes_sent - last_sent
+        recv_delta=bytes_recv - last_recv
+        self._last_sample = (bytes_sent, bytes_recv)
+        self._last_sample_ts = datetime.datetime.now()
+        delta=(sent_delta,recv_delta)
+        self._buffer.append(delta)
+
+    def copy(self):
+        return self._buffer.copy()
+
+
+class Periodic:
+    def __init__(self, func, time):
+        self._func = func
+        self._time = time
+        self._is_started = False
+        self.__task = None
+
+    async def start(self):
+       if not self._is_started:
+           self._is_started = True
+           # Start task to call func periodically:
+           self.__task = asyncio.ensure_future(self._run())
+
+    async def stop(self):
+       if self._is_started:
+           self._is_started = False
+           # Stop task and await it stopped:
+           self.__task.cancel()
+           with suppress(asyncio.CancelledError):
+                await self.__task
+
+    async def _run(self):
+        while True:
+           await asyncio.sleep(self._time)
+           try:
+                self._func()
+           except Exception as e:
+                traceback.print_exc(e)
+
+
+class Graph:
+
+    def __init__(self, ifname, size, sampler):
+        self._ifname=ifname
+        self._size=size
+        self._img = Image.new(mode="RGBA", size=size)
+        self._drawable = ImageDraw.Draw(self._img)
+        self._sampler = sampler
+        self._max = 10
+
+    def draw(self):
+        samples = self._sampler.copy()
+        series = [x for (x,_) in samples]
+        self._max= max([*series,self._max])
+        # normalize
+        scaled_samples = [x/self._max for x in series]
+        (w,h) = self._size
+        heights=[ x*h for x in scaled_samples]
+        # Clear viewport
+        self._drawable.rectangle([0,0,w,h],fill=getrgb("black"))
+        s_x=0
+        white = getrgb("white")
+        for sample in heights:
+            self._drawable.line([s_x,h,s_x,h-sample],fill=white)
+            s_x = s_x + 1
+        return self._img
+
+
+class Screen:
+
+    def __init__(self, display, widgets):
+        self._display = display
+        self._widgets = widgets
+        self._periodic = Periodic(self._draw,5)
+        self._screen = Image.new(mode="RGBA",size=fb.size)
+        screen_drawable = ImageDraw.Draw(self._screen)
+        screen_drawable.text((10, 10), "eth0.2", fill=getrgb("white"))
+        (x,y) = display.size
+        screen_drawable.rectangle([0,0,x-1,y-1], outline=getrgb("white"), fill=None, width=2)
+        screen_data = self._screen.tobytes()
+        display.write_screen(screen_data)
+
+    def _draw(self):
+        for (widget, viewport) in self._widgets:
+            img = widget.draw()
+            (x,y,_,_) = viewport
+            self._screen.alpha_composite(img,(x,y))
+        self._display.write_screen(self._screen.tobytes())
+
+    async def start(self):
+        await self._periodic.start()
+
+
+
+
+if __name__ == "__main__":
+    fb = Framebuffer()
+    #print(f"{fb.size} {fb.bpp}")
+    scraper = IfSampler("eth0.2", 800)
+    with fb as display:
+        display.clear((128, 128, 128,255))
+
+        graph = Graph("eth0.2", (800,80), scraper)
+        widgets = [(graph, (40,40,800,80))]
+        screen = Screen(display, widgets)
+
+        loop=asyncio.get_event_loop()
+        loop.set_debug(True)
+        loop.create_task(scraper.start())
+        loop.create_task(screen.start())
+        loop.run_forever()
